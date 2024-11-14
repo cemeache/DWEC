041996c9e131b4de48548990a4f91cd50dfbbe2a
Merroji <156344132+merroji@users.noreply.github.com>
2024-11-14 19:45:36 +0100

    Arreglo Mostrar Avatar

--- 00JavaScript.info/asyncAwait/script.js
+++ 00JavaScript.info/asyncAwait/script.js
@@ -1,16 +1,15 @@
 async function verAvatar() {
     // leer usuario github
-    let response = await fetch(`https://api.github.com/users/albdom222`);
+    let response = await fetch(`https://api.github.com/users/cemeache`);
     let user = await response.json();
 
     //Mostrar Avatar
     let img = document.createElement("img");
-    img.scr = user.avatar_url;
-    img.className = "avatar-alberto";
+    img.src = user.avatar_url;
+    img.className = "avatar-cemeache";
     document.body.append(img);
 
     return response;
-
 }
 
 verAvatar();
