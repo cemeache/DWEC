17bf071e30f6f916ff3c1815d0523179aca924b4
Merroji <156344132+merroji@users.noreply.github.com>
2024-11-14 20:03:59 +0100

    Aprendizaje Realizado

---
+++ 00JavaScript.info/asyncAwait/01Aprendizaje/script.js
@@ -0,0 +1,13 @@
+async function verAvatar() {
+    const response = await fetch('https://api.github.com/users/cemeache');
+    const usu = await response.json();
+
+    let img = document.createElement('img');
+    img.src = usu.avatar_url;
+    img.className = "avatar-celia";
+    document.body.append(img);
+
+    console.log(response);
+}
+
+verAvatar();
